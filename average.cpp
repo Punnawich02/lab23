1a6329d33d2642e9a109d5443d01cad7bfe80bcf
Punnawich Mai <punnawich_dachin@gmail.com>
2023-03-03 14:58:21 +0700

    Finished

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc ,char *argv[])
+{
+    if(argc == 1)
+    {
+        cout << "Please input numbers to find average.";
+    }
+    else
+    {
+        cout << "---------------------------------" << endl;
+        double sum;
+        for(int i = 0;i<argc;i++)
+        {
+            sum += atof(argv[i]);
+        }
+        cout << "Average of " << argc + 1 << "numbers = " << sum/(double(argc) - 1) << endl; 
+        cout << "---------------------------------";
+    }
+}
