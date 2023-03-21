2ef696d73b190c3d08d1de1fe9efda6e66fb11ae
Punnawich Mai <punnawich_dachin@gmail.com>
2023-03-21 16:45:35 +0700

    finished

--- calculator.cpp
+++ calculator.cpp
@@ -1,9 +1,71 @@
 #include <windows.h>
+#include <stdio.h>
+
+HWND field1, field2, Head;
+HWND plus, diff, multiply ,divide;
+char szClassName[] = "TextEntry";
+char textSaved[100];
+double number;
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
 		
+		case WM_CREATE:
+			Head = CreateWindow("STATIC", "Please input two numbers", WS_VISIBLE | WS_CHILD , 15, 10, 200, 25, hwnd, NULL ,NULL ,NULL); // x,y,width,high
+			field1 = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER, 65, 40, 100, 20, hwnd, NULL ,NULL ,NULL);
+			field2 = CreateWindow("EDIT", "", WS_BORDER | WS_CHILD | WS_VISIBLE, 65, 65, 100, 20, hwnd, NULL, NULL, NULL);
+			plus = CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD , 60, 100, 20, 20, hwnd, (HMENU) 1, NULL, NULL);
+			diff = CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD , 90, 100, 20, 20, hwnd, (HMENU) 2, NULL, NULL);
+			multiply = CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD , 120, 100, 20, 20, hwnd, (HMENU) 3, NULL, NULL);
+			divide = CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD , 150, 100, 20, 20, hwnd, (HMENU) 4, NULL, NULL);
+		break;
+
+		case WM_COMMAND:
+
+			switch (LOWORD(wParam))
+			{
+				case 1: // plus
+					GetWindowText(field1, textSaved, sizeof(textSaved));
+					number = std::atof(textSaved);
+					GetWindowText(field2, textSaved, sizeof(textSaved));
+					number += std::atof(textSaved);
+					::MessageBeep(MB_ICONERROR);
+					sprintf(textSaved,"%f", number);
+					::MessageBox(hwnd, textSaved, "Result", MB_OK);
+					break;
+
+				case 2: // different
+					GetWindowText(field1, textSaved, sizeof(textSaved));
+					number = std::atof(textSaved);
+					GetWindowText(field2, textSaved, sizeof(textSaved));
+					number -= std::atof(textSaved);
+					::MessageBeep(MB_ICONERROR);
+					sprintf(textSaved,"%f", number);
+					::MessageBox(hwnd, textSaved, "Result", MB_OK);
+					break;
+
+				case 3: // multiply
+					GetWindowText(field1, textSaved, sizeof(textSaved));
+					number = std::atof(textSaved);
+					GetWindowText(field2, textSaved, sizeof(textSaved));
+					number *= std::atof(textSaved);
+					::MessageBeep(MB_ICONERROR);
+					sprintf(textSaved,"%f", number);
+					::MessageBox(hwnd, textSaved, "Result", MB_OK);
+					break;
+
+				case 4: // divide
+					GetWindowText(field1, textSaved, sizeof(textSaved));
+					number = std::atof(textSaved);
+					GetWindowText(field2, textSaved, sizeof(textSaved));
+					number /= std::atof(textSaved);
+					::MessageBeep(MB_ICONERROR);
+					sprintf(textSaved,"%f", number);
+					::MessageBox(hwnd, textSaved, "Result", MB_OK);
+					break;
+			}
+			break;
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
 			PostQuitMessage(0);
@@ -31,7 +93,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+19);
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
@@ -41,11 +103,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","Caption",WS_VISIBLE|WS_OVERLAPPEDWINDOW,
+	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,"WindowClass","My Calculator",WS_VISIBLE|WS_SYSMENU,
 		CW_USEDEFAULT, /* x */
 		CW_USEDEFAULT, /* y */
-		640, /* width */
-		480, /* height */
+		250, /* width */
+		200, /* height */
 		NULL,NULL,hInstance,NULL);
 
 	if(hwnd == NULL) {
